9580b778689481f67908022d510189780f93f323
ShohruhUzDev <shohruh7464784@mail.ru>
2023-03-30 02:28:38 +0500

    Add project files.

---
+++ DemoCenter.Web/Shared/NavMenu.razor.cs
@@ -0,0 +1,12 @@
+namespace DemoCenter.Web.Shared
+{
+    public partial class NavMenu
+    {
+        private bool collapseNavMenu = true;
+        private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
