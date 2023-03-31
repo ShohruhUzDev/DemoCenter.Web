d43af6e0e58437e557aa9833ff6914898d3cc351
ShohruhUzDev <shohruh7464784@mail.ru>
2023-04-01 01:26:42 +0500

    FOUNDATIONS : Foundations RetrileveAll Users

--- DemoCenter.Web/Brokers/ApiBrokers/ApiBroker.Users.cs
+++ DemoCenter.Web/Brokers/ApiBrokers/ApiBroker.Users.cs
@@ -4,7 +4,23 @@ namespace DemoCenter.Web.Brokers.ApiBrokers
 {
     public partial class ApiBroker
     {
+        private const string UsersRelativeUrl = "api/users";
+
+        public async ValueTask<User> PostUserAsync(User user) =>
+            await PostAsync<User>(UsersRelativeUrl, user);
+
         public async ValueTask<List<User>> GetAllUsersAsync() =>
-            await GetAsync<List<User>>("api/Users");
+            await GetAsync<List<User>>(UsersRelativeUrl);
+
+        public async ValueTask<User> GetUserByIdAsync(Guid userId) =>
+            await GetAsync<User>($"{UsersRelativeUrl}/{userId}");
+
+        public async ValueTask<User> PutUserAsync(User user) =>
+            await PutAsync<User>(UsersRelativeUrl, user);
+
+        public async ValueTask<User> DeleteUserAsync(Guid userId) =>
+            await DeleteAsync<User>($"{UsersRelativeUrl}/{userId}");
+
+
     }
 }
---
+++ DemoCenter.Web/Services/Foundations/Users/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using DemoCenter.Web.Models.Users;
+
+namespace DemoCenter.Web.Services.Foundations.Users
+{
+    public interface IUserService
+    {
+        public ValueTask<List<User>> RetrieveAllUsersAsync();
+    }
+}
