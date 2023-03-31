0a2f73f271c74c08770db3c5445091e4557e5cc7
ShohruhUzDev <shohruh7464784@mail.ru>
2023-04-01 01:26:42 +0500

    FOUNDATIONS : Foundations RetrileveAll Users

---
+++ DemoCenter.Web/Services/Foundations/Users/UserService.cs
@@ -0,0 +1,17 @@
+﻿using DemoCenter.Web.Brokers.ApiBrokers;
+using DemoCenter.Web.Models.Users;
+
+namespace DemoCenter.Web.Services.Foundations.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly IApiBroker apiBroker;
+
+        public UserService(IApiBroker apiBroker) =>
+            this.apiBroker = apiBroker;
+
+        public async ValueTask<List<User>> RetrieveAllUsersAsync() =>
+            await this.apiBroker.GetAllUsersAsync();
+
+    }
+}
