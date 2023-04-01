add44f85a63c1c51a49661a574d8d06a42888929
ShohruhUzDev <shohruh7464784@mail.ru>
2023-04-01 23:02:39 +0500

    DATA : Createa Subject Model

---
+++ DemoCenter.Web/Models/subjects/Subject.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoCenter.Web.Models.Subjects
+{
+    public class Subject
+    {
+        public Guid Id { get; set; }
+        public string SubjectName { get; set; }
+        public int Price { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+
+     //   public virtual IEnumerable<Group> Groups { get; set; }
+    }
+}
