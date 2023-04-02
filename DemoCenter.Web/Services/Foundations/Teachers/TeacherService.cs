7d3af40e44d704a1a7c6db61e79d7caab00a8200
ShohruhUzDev <shohruh7464784@mail.ru>
2023-04-02 17:42:04 +0500

    FOUNDATIONS : Retrieve All Teacher

---
+++ DemoCenter.Web/Services/Foundations/Teachers/TeacherService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DemoCenter.Web.Brokers.ApiBrokers;
+using DemoCenter.Web.Models.Teachers;
+
+namespace DemoCenter.Web.Services.Foundations.Teachers
+{
+    public class TeacherService
+    {
+        private readonly IApiBroker apiBroker;
+
+        public async ValueTask<List<Teacher>> RetrieveAllTeachersAsync() =>
+            await apiBroker.GetAllTeachersAsync();
+    }
+}
