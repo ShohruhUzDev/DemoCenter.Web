0680fe170cf107af3406c5da4eaa62ecf0b6dea3
ShohruhUzDev <shohruh7464784@mail.ru>
2023-04-02 17:54:02 +0500

    FOUNDATIONS :  Retrieve All TeacherView

---
+++ DemoCenter.Web/Services/Views/TeacherViews/ITeacherViewService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DemoCenter.Web.Models.TeacherViews;
+
+namespace DemoCenter.Web.Services.Views.TeacherViews
+{
+    public interface ITeacherViewService
+    {
+        public ValueTask<List<TeacherView>> RetrieveAllTeacherViewsAsync();
+    }
+}
