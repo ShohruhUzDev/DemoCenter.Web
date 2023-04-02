c99536812c31ed942b8a0d02598bd91dd885707c
ShohruhUzDev <shohruh7464784@mail.ru>
2023-04-02 17:16:55 +0500

    BROKERS : Add Teacher Methods

---
+++ DemoCenter.Web/Brokers/ApiBrokers/IApiBroker.Teachers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DemoCenter.Web.Models.Teachers;
+
+namespace DemoCenter.Web.Brokers.ApiBrokers
+{
+    public partial interface IApiBroker
+    {
+        ValueTask<Teacher> PostTeacherAsync(Teacher teacher);
+        ValueTask<List<Teacher>> GetAllTeachersAsync();
+        ValueTask<Teacher> GetTeacherByIdAsync(Guid teacherId);
+        ValueTask<Teacher> PutTeacherAsync(Teacher teacher);
+        ValueTask<Teacher> DeleteTeacherAsync(Guid teacherId);
+    }
+}
